23ac104356b9e866205c40f1710bf2b64abfb993
Chandrakant <chandrakantgawali92@gmail.com>
2023-09-30 12:48:08 +0530

    Add Hosted backend Links

--- src/scenes/widgets/PostsWidget.jsx
+++ src/scenes/widgets/PostsWidget.jsx
@@ -12,7 +12,7 @@ const PostsWidget = ({ userId, isProfile = true }) => {
   const getPosts = async () => {
     
     try {
-      const response = await fetch("http://localhost:3001/posts", {
+      const response = await fetch("https://foa-social-backend3.onrender.com/posts", {
         method: "GET",
         headers: { Authorization: `Bearer ${token}` },
       });
@@ -25,7 +25,7 @@ const PostsWidget = ({ userId, isProfile = true }) => {
 
   const getUserPosts = async () => {
     const response = await fetch(
-      `http://localhost:3001/posts/${userId}/posts`,
+      `https://foa-social-backend3.onrender.com/posts/${userId}/posts`,
       {
         method: "GET",
         headers: { Authorization: `Bearer ${token}` },
